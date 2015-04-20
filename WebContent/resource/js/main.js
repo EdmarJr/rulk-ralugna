564e6b2477c397520d7f2b6cc0470e9d4871ffc2
c1281452 <c1281452@ESBSA069910PL56.df.intrabb.bb.com.br>
2015-04-20 17:44:34 -0300

    ajustes

--- WebContent/resource/js/main.js
+++ WebContent/resource/js/main.js
@@ -1,254 +1,232 @@
-const enderecoRest = '/Rest-web/rest';
-var app = angular.module('academia', ['smart-table','ngRoute','ngCookies','restangular']);
+const
+enderecoRest = '/Rest-web';
+var app = angular.module('academia', [ 'smart-table', 'ngRoute', 'ngCookies',
+		'restangular' ]);
 
-app.factory('Auth',['$cookieStore',function($cookieStore) {
-	var _user = $cookieStore.get('user');
-	
-	var setUser = function(user) {
-		
-		if(user.roles && user.roles[0] == "ADMIN") {
-			user.role = 10;
-		}
-		if(!user.role || user.role < 0) {
-			user.role = 1;
-		}
-		_user = user;
-		$cookieStore.put('user',_user);
-	}
-	
-	if(!_user) {
-		setUser({role : 1});
-	}
-	
-	return {
-		isAuthorized: function(lvl) {
-			return _user.role >= lvl;
-		},
-		setUser: setUser,
-		isLoggedIn: function() {
-			return _user.role > 1 ;
-		},
-		getUser: function() {
-			return _user;
-		},
-		getId: function() {
-			return _user ? _user._id : null;
-		},
-		getToken: function() {
-			return _user ? _user.token : '';
-		},
-		logout: function() {
-			$cookieStore.remove('user');
-			_user = null;
-		}
-	}
+app.factory('Auth', [
+		'$cookieStore',
+		'Restangular',
+		function($cookieStore, Restangular) {
 
-}]);
+			return {
+				getUsuarioLogado : function(callback) {
+					Restangular.one("usuarios/logado").get().then(
+							function(retorno) {
+								console.log(retorno);
+								callback(retorno);
+							}, function(err) {
+								console.log(err);
+							});
+				},
+				isUsuarioLogadoAuthorized : function(callback) {
+					this.getUsuarioLogado(function(usuarioLogado, err) {
+						if (usuarioLogado) {
+							callback(usuarioLogado);
+						} else {
+							callback(undefined, {
+								msg : "usuario não está logado"
+							});
+						}
+					});
+				}
+			}
+
+			// var setUser = function(user) {
+			//		
+			// if(user.roles && user.roles[0] == "ADMIN") {
+			// user.role = 10;
+			// }
+			// if(!user.role || user.role < 0) {
+			// user.role = 1;
+			// }
+			// _user = user;
+			// $cookieStore.put('user',_user);
+			// }
+			//	
+			// if(!_user) {
+			// setUser({role : 1});
+			// }
+			//	
+			// return {
+			// isAuthorized: function(lvl) {
+			// return _user.role >= lvl;
+			// },
+			// setUser: setUser,
+			// isLoggedIn: function() {
+			// return _user.role > 1 ;
+			// },
+			// getUser: function() {
+			// return _user;
+			// },
+			// getId: function() {
+			// return _user ? _user._id : null;
+			// },
+			// getToken: function() {
+			// return _user ? _user.token : '';
+			// },
+			// logout: function() {
+			// $cookieStore.remove('user');
+			// _user = null;
+			// }
+			// }
+
+		} ]);
 
 app.run(function($rootScope, $location, Auth) {
-	$rootScope.$on('$routeChangeStart', function(evt,next,curr) {
-		if (!Auth.isAuthorized(next.access_level)) {
-			if (Auth.isLoggedIn()) {
-			// The user is logged in, but does not
-			// have permissions to view the view
-				$location.path('/');
+	$rootScope.$on('$routeChangeStart', function(evt, next, curr) {
+		Auth.isUsuarioLogadoAuthorized(function(usuarioLogado, err) {
+			if (err) {
+				evt.preventDefault();
+				$rootScope.$evalAsync(function() {
+					$location.path('/login');
+				});
 			} else {
-				$location.path('/login');
+				evt.preventDefault();
+				$rootScope.$evalAsync(function() {
+					$location.path('/');
+				});
 			}
-		}
-	});
+		});
+	})
 });
 
-app.config(['$routeProvider',function($routeProvider) {
-    $routeProvider.when('/cadastro/empresa', {
-        templateUrl: 'empresa/formularioEmpresa.html',
-        controller: 'FormularioEmpresaController',
-        access_level: 1
-    }).when('/cadastro/unidade',{
-    	templateUrl: 'unidade/formularioUnidade.html',
-    	controller: 'FormularioUnidadeController',
-    	access_level: 8
-    }).when('/cadastro/usuario',{
-    	templateUrl: 'usuario/formularioUsuario.html',
-    	controller: 'FormularioUsuarioController',
-    	access_level: 3
-    }).when('/login',{
-    	templateUrl: 'login.html',
-    	controller: 'FormLoginController',
-    	access_level: 1
-    }).when('/autenticado/inicio',{
-    	templateUrl: 'autenticado/inicio.html',
-    	controller: 'InicioController',
-    	access_level: 2
-    }).when('/autenticado/administrativo/cadastro/cliente',{
-    	templateUrl: 'autenticado/administrativo/cliente/formCliente.html',
-    	controller: 'CadastroClienteController',
-    	access_level: 2
-    }).when('/autenticado/administrativo/consulta/clientes',{
-    	templateUrl: 'autenticado/administrativo/cliente/showClientes.html',
-    	controller: 'ConsultaClienteController',
-    	access_level: 2
-    }).otherwise({
-    	redirectTo: '/autenticado/inicio'
-    })
-}]);
+app.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/cadastro/empresa', {
+		templateUrl : 'empresa/formularioEmpresa.html',
+		controller : 'FormularioEmpresaController',
+		access_level : 1
+	}).when('/cadastro/unidade', {
+		templateUrl : 'unidade/formularioUnidade.html',
+		controller : 'FormularioUnidadeController',
+		access_level : 8
+	}).when('/cadastro/usuario', {
+		templateUrl : 'usuario/formularioUsuario.html',
+		controller : 'FormularioUsuarioController',
+		access_level : 3
+	}).when('/login', {
+		templateUrl : 'login.html',
+		controller : 'FormLoginController',
+		access_level : 1
+	}).when('/autenticado/inicio', {
+		templateUrl : 'autenticado/inicio.html',
+		controller : 'InicioController',
+		access_level : 2
+	}).when('/autenticado/administrativo/cadastro/cliente', {
+		templateUrl : 'autenticado/administrativo/cliente/formCliente.html',
+		controller : 'CadastroClienteController',
+		access_level : 2
+	}).when('/autenticado/administrativo/consulta/clientes', {
+		templateUrl : 'autenticado/administrativo/cliente/showClientes.html',
+		controller : 'ConsultaClienteController',
+		access_level : 2
+	}).otherwise({
+		redirectTo : '/autenticado/inicio'
+	})
+} ]);
 
 app.config(function(RestangularProvider) {
 	RestangularProvider.setBaseUrl(enderecoRest);
 });
 
-app.factory('UnidadeService',['Restangular',function(Restangular) {
+app.factory('UnidadeService', [
+		'Restangular',
+		function(Restangular) {
+			return {
+				objRest : Restangular.all('secured/unidades'),
+				post : function(unidade) {
+					this.objRest.post(unidade).then(function(resp) {
+						window.alert('deu certo');
+					}, function(err) {
+						window.alert('deu erro');
+					});
+				},
+				getUnidadesDisponiveis : function(promise) {
+					this.objRest.getList().then(promise, function(err) {
+						window.alert("deu erro");
+					});
+				},
+				getPlanosDisponiveis : function(unidade, callback) {
+					Restangular.one('unidades', unidade.id).getList('planos')
+							.then(callback, function(err) {
+								window.alert("erro");
+							});
+				}
+			}
+		} ]);
+
+app.factory('ClienteService', [ 'Restangular', function(Restangular) {
 	return {
-		objRest : Restangular.all('unidades'),
-		post : function(unidade) {
-			this.objRest.post(unidade).then(function(resp) {
-				window.alert('deu certo');
-			},function(err) {
+		objRest : Restangular.all('clientes'),
+		post : function(cliente, callback) {
+			cliente.type = "com.rest.entitys.Cliente"
+			this.objRest.post(cliente).then(callback, function(err) {
+
+			});
+		}
+	}
+} ]);
+
+app.factory('LoginService', function($http, Restangular, Auth, $location) {
+	function redirecionar(endereco) {
+		$location(endereco);
+	}
+	
+	
+	return {
+		logar : function(user) {
+			var $location = $location;
+			$http.post('/Rest-web/login', {
+				email : user.email,
+				senha : user.senha
+			}).success(function(data, status, headers, config) {
+				redirecionar('/autenticado/inicio');
+			}).error(function(data, status, headers, config) {
 				window.alert('deu erro');
 			});
-		},
-		getUnidadesDisponiveis : function(promise) {
-			this.objRest.getList().then(promise,function(err) {
-				window.alert("deu erro");
-			});
-		},
-		getPlanosDisponiveis: function(unidade, callback) {
-			Restangular.one('unidades',unidade.id).getList('planos').then(callback,function(err) {
-				window.alert("erro");
-			});
-		}
-	}
-}]);
-
-app.factory('ClienteService',['Restangular',function(Restangular) {
-	return {
-		objRest: Restangular.all('clientes'),
-		post: function(cliente,callback) {
-			cliente.type = "com.rest.entitys.Cliente"
-			this.objRest.post(cliente).then(callback,function(err) {
-				
-			}); 
-		}
-	}
-}]);
-
-app.factory('LoginService',function($http,Restangular,Auth,$location) {
-	return {
-		logar: function(user)  {
-			 $http.post('/Rest-web/j_security_check', user).success(function(data, status, headers, config) {
-			    	window.alert('logou');
-			  }).error(function(data, status, headers, config) {
-				    window.alert('deu erro');
-			  });
 		}
 	}
 });
 
-app.factory("PlanoService",['Restangular',function(Restangular) {
+app.factory("PlanoService", [ 'Restangular', function(Restangular) {
 	return {
-		objRest: Restangular.all('planos'),
-		
+		objRest : Restangular.all('planos'),
+
 	}
-}]);
+} ]);
 
-
-app.controller('FormLoginController',function($scope,LoginService) {
+app.controller('FormLoginController', function($scope, LoginService) {
 	$scope.submitForm = function() {
 		LoginService.logar($scope.user);
 	}
 });
 
-app.controller('InicioController',function($scope,$route,$location) {
+app.controller('InicioController', function($scope, $route, $location) {
 	$scope.irParaAdministracao = function() {
 		$location.path('/autenticado/administrativo/cadastro/cliente');
 	}
 });
 
-app.controller('CadastroClienteController',function($scope,$route,$location,UnidadeService,ClienteService) {
-	UnidadeService.getUnidadesDisponiveis(function (resp) {
+app.controller('CadastroClienteController', function($scope, $route, $location,
+		UnidadeService, ClienteService) {
+	UnidadeService.getUnidadesDisponiveis(function(resp) {
 		$scope.unidades = resp;
 	});
-	
+
 	$scope.atualizarPlanos = function() {
-		UnidadeService.getPlanosDisponiveis($scope.cliente.unidade,function(resp) {
+		UnidadeService.getPlanosDisponiveis($scope.cliente.unidade, function(
+				resp) {
 			$scope.planos = resp;
 		})
 	};
-	
+
 	$scope.cadastrarCliente = function() {
-		ClienteService.post($scope.cliente,function(resp) {
+		ClienteService.post($scope.cliente, function(resp) {
 			window.alert("foi bonito foooi");
 		});
 	}
 });
 
-app.controller('ConsultaClienteController',['$scope', '$timeout',function($scope,$timeout) {
-	
-	var nameList = ['Pierre', 'Pol', 'Jacques', 'Robert', 'Elisa','Edmar','Maria','Teste','Goku','GOHAN'];
-    var familyName = ['Dupont', 'Germain', 'Delcourt', 'bjip', 'Menez','Fagundes','teste','meneguel'];
+app.controller('ConsultaClienteController', [ '$scope', '$timeout',
+		function($scope, $timeout) {
 
-    $scope.isLoading = false;
-    $scope.rowCollection = [];
-
-
-    function createRandomItem() {
-        var
-            firstName = nameList[Math.floor(Math.random() * 4)],
-            lastName = familyName[Math.floor(Math.random() * 4)],
-            age = Math.floor(Math.random() * 100),
-            email = firstName + lastName + '@whatever.com',
-            balance = Math.random() * 3000;
-
-        return {
-            firstName: firstName,
-            lastName: lastName,
-            age: age,
-            email: email,
-            balance: balance
-        };
-    }
-
-    function getAPage() {
-        var data = [];
-        for (var j = 0; j < 20; j++) {
-            data.push(createRandomItem());
-        }
-        return data;
-    }
-
-    var lastStart = 0;
-    var maxNodes = 40;
-
-    $scope.callServer = function getData(tableState) {
-
-        //here you could create a query string from tableState
-        //fake ajax call
-        $scope.isLoading = true;
-
-        $timeout(function () {
-
-            //if we reset (like after a search or an order)
-            if (tableState.pagination.start === 0) {
-                $scope.rowCollection = getAPage();
-            } else {
-                //we load more
-                $scope.rowCollection = $scope.rowCollection.concat(getAPage());
-
-                //remove first nodes if needed
-                if (lastStart < tableState.pagination.start && $scope.rowCollection.length > maxNodes) {
-                    //remove the first nodes
-                    $scope.rowCollection.splice(0, 20);
-                }
-            }
-
-            lastStart = tableState.pagination.start;
-
-            $scope.isLoading = false;
-        }, 1000);
-
-    };
-
-    $scope.rowCollection = getAPage();
-}]);
-
-
+		} ]);
